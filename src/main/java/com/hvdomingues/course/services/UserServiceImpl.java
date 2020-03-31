c7b4fe2ed3b14475c438297ad051dfce7a89d28e
hvdomingues <hvdomingues1@gmail.com>
2020-03-31 11:27:10 -0300

    Classe UserService mudada para interface e criada classe UserServiceImpl

---
+++ src/main/java/com/hvdomingues/course/services/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.hvdomingues.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hvdomingues.course.entities.User;
+import com.hvdomingues.course.repositories.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	@Override
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
