4a3e332cbb9f76f8920fd1d49ea34acbe0ad4899
hvdomingues <hvdomingues1@gmail.com>
2020-04-04 01:01:32 -0300

    Exception handling - delete

--- src/main/java/com/hvdomingues/course/services/UserServiceImpl.java
+++ src/main/java/com/hvdomingues/course/services/UserServiceImpl.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.hvdomingues.course.entities.User;
 import com.hvdomingues.course.repositories.UserRepository;
+import com.hvdomingues.course.services.exceptions.DatabaseException;
 import com.hvdomingues.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -35,7 +38,15 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	@Override
